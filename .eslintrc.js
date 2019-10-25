7ed14f4235e2632dc1702f34c61f3ee741c9f4fa
Paul Schmalenberg <paul.schmalenberg@growthzone.com>
2019-10-24 22:30:59 -0500

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,17 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es6": true
+        "es6": true,
+        jquery: true
     },
     "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+        "SharedArrayBuffer": "readonly",
+        "MNI": "readonly",
+        "CMS": "readonly",
+        "MMP": "readonly",
+        "MIC": "readonly"
     },
     "parserOptions": {
         "ecmaVersion": 2018,
---
+++ js/defaults.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    //
+    $('a').att('class','new-link')
+})
